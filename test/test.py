2505418eae94daa3811a6c20a7ef5fa74bd50253
wmchappy <fghjklzxcwang@foxmail.com>
2019-01-06 16:05:18 +0800

    first commit

--- test/test.py
+++ test/test.py
@@ -1,3 +1,9 @@
 import werobot
-
+robot = werobot.WeRoBot(token='daixiangzi')  #token="daixiangzi"
+@robot.text
+def echo(message):
+    return message.content
+robot.config['HOST'] = '0.0.0.0' #默认允许所有IP访问
+robot.config['PORT'] = 80  #指定80端口
+robot.run()
 
