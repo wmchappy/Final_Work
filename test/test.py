5f92d941d3e5f0cf5453a241d835c6314b15ecea
wmchappy <fghjklzxcwang@foxmail.com>
2019-01-06 17:06:04 +0800

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,9 +1,10 @@
 import werobot
-robot = werobot.WeRoBot(token='daixiangzi')  #token="daixiangzi"
+
+robot = werobot.WeRoBot(token='tokenhere')
+
 @robot.text
-def echo(message):
-    return message.content
-robot.config['HOST'] = '0.0.0.0' #默认允许所有IP访问
-robot.config['PORT'] = 80  #指定80端口
+def hello_world():
+    return 'Hello World!'
+
 robot.run()
 
