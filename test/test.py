be2240a61513b26c3fd626be67c5f303def0f663
root <root@localhost.localdomain>
2019-01-08 16:24:06 +0800

    next

--- test/test.py
+++ test/test.py
@@ -1,10 +1,15 @@
+#!/usr/bin/python
+#vim: set fileencoding:utf-8
+
 import werobot
-
-robot = werobot.WeRoBot(token='tokenhere')
-
+robot = werobot.WeRoBot(token='tokenhere')  #token="tokenhere"
 @robot.text
-def hello_world():
-    return 'Hello World!'
-
+def echo(message):
+    a = '<a href=\'http://class.710071.net?wechatid=\".$fromUsername.\"\'>点击进入西电微信课堂</a>'
+    if message.content ==  "hello" :
+        return a 
+    else:
+        return '请输入hello'
+robot.config['HOST'] = '0.0.0.0'
+robot.config['PORT'] = 80
 robot.run()
-
